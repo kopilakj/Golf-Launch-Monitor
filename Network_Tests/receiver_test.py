2b6991b1a214b919f406b60d78b1491807003b90
Jesse <kopilakj35@gmail.com>
2026-03-06 00:20:21 +0000

    Networking files

--- Network_Tests/receiver_test.py
+++
@@ -1,9 +0,0 @@
-import socket
-LISTEN_IP = "0.0.0.0"
-LISTEN_PORT = 5005
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((LISTEN_IP, LISTEN_PORT))
-
-print(f"listening on {LISTEN_IP}:{LISTEN_PORT}")
-data, addr = sock.recvfrom(2048)
-print(f"received from {addr}: {data}")
