81b36ab9b619db2ef97877631ee4d3de40076e99
Elizabeth Lumbis <elizabethlumbis@gmail.com>
2026-03-07 17:20:40 -0500

    Added new CSV to JSON functionality and updated GUI

--- WebApplication/pi_client/send_metrics.py
+++ WebApplication/pi_client/send_metrics.py
@@ -2,52 +2,71 @@ import json
 import urllib.request
 import os
 import requests
+import csv
 
-url = "http://10.42.0.233:5000/api/upload_metrics"
+url = "http://127.0.0.1:5000/api/upload_metrics"
 
-def get_club():	
-	try:
-		response = requests.get("http://127.0.0.1:5001/get_club")
-		club = response.json()["club"]
+def get_club():
+    try:
+        response = requests.get("http://127.0.0.1:5001/get_club")
+        club = response.json()["club"]
 
-		if club is None:
-			print("No club selected yet.")
-			return None
-		return club
+        if club is None:
+            print("No club selected yet.")
+            return None
+        return club
 
-	except Exception as e:
-		print("Error getting club from pi server:", e)
-		return None
+    except Exception as e:
+        print("Error getting club from pi server:", e)
+        return None
+
+
+def load_metrics_from_csv(csv_path):
+    with open(csv_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            data = {
+                "ball_speed": float(row["ball_speed_pxps"]),
+                "launch_angle": float(row["launch_angle_deg"]),
+                "apex_height": float(row["apex_height_px"]),
+                "carry_distance": float(row["carry_px"]),
+                "vx": float(row["vx_pxps"]),
+                "vy": float(row["vy_pxps"])
+            }
+
+            return data
+
 
 def send_metrics():
-	club = get_club()
+    club = get_club()
 
-	if club is None:
-		print("Cannot send metrics, no club selected.")
-		return
+    if club is None:
+        print("Cannot send metrics, no club selected.")
+        return
 
-	data = {"club_speed": 110,
-		"ball_speed": 160,
-		"launch_angle": 14.5,
-		"spin_rate": 3200,
-		"carry_distance": 265,
-		"total_distance": 280
-	}
+    data = load_metrics_from_csv("metrics.csv")
 
-	json_data = json.dumps(data).encode("utf-8")
-	
-	req = urllib.request.Request(
-		url,
-	 	data=json_data,
-	 	headers={"Content-Type": "application/json"}
-	)
+    if data is None:
+        print("No metrics found.")
+        return
 
-	try:
-		with urllib.request.urlopen(req, timeout=5) as response:
-			print("Server response:", response.read().decode())
-	except Exception as e:
-		print("Error sending metrics:", e)
+    data["club"] = club
+
+    json_data = json.dumps(data).encode("utf-8")
+
+    req = urllib.request.Request(
+        url,
+        data=json_data,
+        headers={"Content-Type": "application/json"}
+    )
+
+    try:
+        with urllib.request.urlopen(req, timeout=5) as response:
+            print("Server response:", response.read().decode())
+    except Exception as e:
+        print("Error sending metrics:", e)
+
 
 if __name__ == "__main__":
-	send_metrics()
-
+    send_metrics()
