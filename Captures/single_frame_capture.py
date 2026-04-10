600c728b990aa8af0b8f95e2b5db3df3b303efb2
kopilakj <kopilakj35@gmail.com>
2026-04-10 18:47:23 +0100

    Add

--- Captures/single_frame_capture.py
+++ Captures/single_frame_capture.py
@@ -6,7 +6,7 @@ from picamera2 import Picamera2
 SIZE = (640, 400)
 TARGET_FPS = 300
 EXPOSURE_US = 500
-ANALOGUE_GAIN = 4.0
+ANALOGUE_GAIN = 8.0
 BUFFER_COUNT = 4
 SAVE_PATH = Path(__file__).resolve().parent / "pov_test.png"
 def main():
